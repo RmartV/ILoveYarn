b39c9af683bc84c1d1dfaa6f30d6cbb52ad80253
RmartV <134070727+RmartV@users.noreply.github.com>
2025-02-15 12:55:22 +0000

    added supabase and css

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
+
+
+
+const app = createApp(App)
+
+app.component('login', Login)
+app.component('Signup', Signup)
+
+
+app.mount('#app')
