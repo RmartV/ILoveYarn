8e574e62785eeae521abe27209d164556f03034f
RmartV <134070727+RmartV@users.noreply.github.com>
2025-02-15 13:25:02 +0000

    added signup router

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
+import Signup from '@/views/Signup.vue';
 import Home from '../views/Home.vue';
 
 const routes = [
   { path: '/', component: Login }, // Default route is Login
+  { path: '/signup', component: Signup },
   { path: '/home', component: Home } // Home page after login
 ];
 
