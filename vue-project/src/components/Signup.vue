d3b80cf5b22cd720b42ae948c70076b6dd426bd8
RmartV <134070727+RmartV@users.noreply.github.com>
2025-02-15 13:49:24 +0000

    .

--- vue-project/src/components/Signup.vue
+++ vue-project/src/components/Signup.vue
@@ -55,7 +55,7 @@
 import { ref } from 'vue';
 import { supabase } from '../lib/supabaseClient'
 
-
+const supabase = createClient('https://iycapcrfimbrwdixnfmt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5Y2FwY3JmaW1icndkaXhuZm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MTk1MzksImV4cCI6MjA1NTE5NTUzOX0.Q83ftUckbZ6Ox6bikQ-SHDZ8sXwxuzuvEFodmMjGOGg')
 
 export default {
   setup() {
